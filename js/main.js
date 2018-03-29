5bd1d20af48d67c9fe334d5eb8e35c5260b3fbda
Andy Walpole <me@andywalpole.me>
2018-03-29 16:08:15 +0100

    new directory for section 2.4

--- js/main.js
+++ js/main.js
@@ -3,3 +3,17 @@
 *    Mastering Data Visualization with D3.js
 *    2.5 - Activity: Adding SVGs to the screen
 */
+
+// select SVG canvas
+let svg = d3.select('#canvas');
+
+// append rectangle
+let rect = svg.append('rect');
+
+rect.attr('x', 25);
+rect.attr('y', 0);
+rect.attr('width', 150);
+rect.attr('height', 60);
+rect.attr('fill', 'blue');
+
+const rect = d3.select('#canvas')
